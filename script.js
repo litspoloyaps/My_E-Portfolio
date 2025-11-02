45b75308df1ec44d44d0093e2494590e1084ffcb
Lito Poloyapoy <159044455+litspoloyaps@users.noreply.github.com>
2025-11-02 20:45:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  toggleButton.textContent = body.classList.contains('dark')
+    ? '☀️ Light Mode'
+    : '🌙 Dark Mode';
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+  const links = document.querySelectorAll(".nav-links a");
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("show");
+    });
+  });
+});
+
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+document.querySelectorAll(".certificate-card").forEach(card => {
+  card.addEventListener("click", () => {
+    const img = card.querySelector("img").src;
+    modal.style.display = "block";
+    modalImg.src = img;
+  });
+});
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
